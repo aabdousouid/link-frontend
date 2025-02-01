07d447d98ac4479174523208f0fb940be50c2852
aabdousouid <abdelwehedsouid@gmail.com>
2025-02-01 23:01:32 +0100

    deploy test

--- src/URLShortener.js
+++ src/URLShortener.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 //const API_BASE_URL = "http://localhost:5000/api/url"; // Update with your backend URL
-const API_BASE_URL = "https://linkk-p590.onrender.com"
+const API_BASE_URL = "https://linkk-p590.onrender.com/api/url"
 function URLShortener() {
   const [longUrl, setLongUrl] = useState("");
   const [shortUrls, setShortUrls] = useState([]);
